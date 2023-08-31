ab7e9bf3777999e588963ebf25e4ed0558839555
Adriano Souza <souzaadriano.contato@gmail.com>
2023-08-31 13:46:55 -0300

    feat(date-tim-format.strategy): a date time format strategies

---
+++ src/core/shared/class/date-time/format-strategies/index.ts
@@ -0,0 +1,16 @@
+import { AbstractDateFormat } from './date-format-strategy.abstract';
+import { DefaultDateFormatStrategy } from './default.strategy';
+import { PtBrDateFormatStrategy } from './pt-br.strategy';
+import { TimestampInSecondsStrategy } from './timestamp-in-seconds.strategy';
+
+const defaultDateFormatStrategy = new DefaultDateFormatStrategy();
+const ptBrDateFormatStrategy = new PtBrDateFormatStrategy();
+const timestampInSecondsStrategy = new TimestampInSecondsStrategy();
+
+export const mappedDateFormatters = new Map<string, AbstractDateFormat>([
+  [defaultDateFormatStrategy.type, defaultDateFormatStrategy],
+  [ptBrDateFormatStrategy.type, ptBrDateFormatStrategy],
+  [timestampInSecondsStrategy.type, timestampInSecondsStrategy],
+]);
+
+export type TMappedDateFormatters = Map<string, AbstractDateFormat>;
