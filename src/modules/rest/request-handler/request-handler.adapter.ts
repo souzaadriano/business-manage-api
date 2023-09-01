bc99605192bf4621504892f147574b3bf91cb50c
Adriano Souza <souzaadriano.contato@gmail.com>
2023-09-01 14:35:06 -0300

    feat(rest-request-handler.adapter): implements a request handler contract

---
+++ src/modules/rest/request-handler/request-handler.adapter.ts
@@ -0,0 +1,28 @@
+import { IUseCase } from '@/core/shared/contracts/use-case.contract';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { RestResponseDTO } from '../dto/rest-response.dto';
+import { RestExceptionHandler } from '../exception-handler/rest-exception-handler.adapter';
+import { IRestRequestHandler, TSession } from './request-handler.contract';
+
+@Injectable()
+export class RestRequestHandler implements IRestRequestHandler {
+  constructor(private readonly _exceptionHandler: RestExceptionHandler) {}
+
+  async handle<INPUT, OUTPUT>(
+    useCase: IUseCase<INPUT, OUTPUT>,
+    input: INPUT,
+    session?: TSession,
+  ): Promise<RestResponseDTO> {
+    try {
+      const output: any = await useCase.execute(this._setSessionToInput(input, session));
+      return new RestResponseDTO({ status: HttpStatus.OK, metadata: {}, output });
+    } catch (error) {
+      return this._exceptionHandler.handle(error);
+    }
+  }
+
+  private _setSessionToInput<INPUT>(input: INPUT, session?: TSession): INPUT {
+    input['session'] = session;
+    return input;
+  }
+}
